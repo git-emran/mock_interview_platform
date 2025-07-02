75889b96f4804557ecaa7370c612c91739f61e02
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-02 13:33:42 +0600

    Homepage interview card design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
   return (
@@ -11,7 +13,7 @@ const Page = () => {
           <h2>Get Interview Ready with AI powered Practice & Feedback</h2>
 
           <p className="text-lg">
-            Practice with an agent and get curated feedback/
+            Practice with an agent and get curated feedback
           </p>
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
@@ -26,21 +28,24 @@ const Page = () => {
           className="max-sm:hidden"
         />
       </section>
-      <section className="flex flex-col gap-6 mt-8" >
+      <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
-        <div className="interviews-section" >
-          <p>
-            You haven&apos;t taken any Interviews yet
 
-          </p>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          <p>You haven&apos;t taken any interviews yet</p>
         </div>
       </section>
-      <section className="flex flex-col gap-6 mt-8" >
+      <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
-        <div className="interviews-section" >
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
           <p>There are no interviews available</p>
         </div>
-
       </section>
     </>
   );
