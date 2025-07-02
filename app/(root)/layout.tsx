152088aa154864cb9f73032ede0b120ff1f16a84
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-02 12:14:12 +0600

    Homepage 1/3

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className="root-layout" >
+    <nav>
+      <Link href='/' className="flex items-center gap-2" />
+      <Image src="/logo.svg" alt="logo" width={38} height={32} />
+      <h2>PrepTalk</h2>
+    </nav>
+    {children}
+  </div>;
 };
 
 export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview Ready with AI powered Practice & Feedback</h2>
 
-export default Page
+          <p className="text-lg">
+            Practice with an agent and get curated feedback/
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8" >
+        <h2>Your Interviews</h2>
+        <div className="interviews-section" >
+          <p>
+            You haven&apos;t taken any Interviews yet
+
+          </p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8" >
+        <h2>Take an Interview</h2>
+        <div className="interviews-section" >
+          <p>There are no interviews available</p>
+        </div>
+
+      </section>
+    </>
+  );
+};
+
+export default Page;
