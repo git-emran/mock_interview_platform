e4c722640279b145c4eb694be8fc04a80b72e666
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-03 20:17:56 +0600

    SessionCookie authaction

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { db } from "@/firebase/admin";
+import { cookies } from "next/headers";
 
+const ONE_WEEK = 60 * 60 * 24 * 7
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
 
@@ -31,4 +33,19 @@ export async function signUp(params: SignUpParams) {
       message: "Failed to create an account",
     };
   }
+}
+export async function setSessionCookie(idToken: string) {
+    
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000
+  })
+
+  cookieStore.set('session', sessionCookie , {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    sameSite: 'lax'
+  })
 }
